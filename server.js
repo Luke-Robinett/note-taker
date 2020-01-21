1cc7317ea51c42c95e983f265ae3b4299c336687
Luke Robinett <lukebootcamp@gmail.com>
2020-01-20 16:38:21 -0800

    Added console.log messages for GET and POST operations for easier debugging.

--- server.js
+++ server.js
@@ -17,13 +17,18 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes
 // API routes
 
 app.get("/api/notes", (req, res) => {
+  console.log("Got new GET request.");
   fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
     if (err) {
+      console.log("Database file doesn't exist. It will be automatically created when the first note is entered and saved.");
       res.json({});
     } else {
       try {
-        res.json(JSON.parse(data));
+        const notes = JSON.parse(data);
+        console.log(`Responding with ${notes.length} notes from the database.`);
+        res.json(notes);
       } catch {
+        console.log("Database file doesn't contain any valid note data.");
         res.json({});
       }
     }
@@ -39,12 +44,11 @@ app.post("/api/notes", function (req, res) {
     // Load any saved notes first
     try {
       notes= JSON.parse(data);
+
+      // Auto increment new note by one based on value of last saved note
       newId = parseInt(notes[notes.length - 1].id) + 1;
-      console.log(`Got ${notes.length} notes from database.`);
-    } catch(err) {
-      // If no notes in file or file doesn't exist, start with empty array
-      notes = [];
-      console.log(err.message);
+      console.log(`Got ${notes.length} note(s) from database.`);
+    } catch {
       console.log("Couldn't get data from database so starting with empty file.");
     }
 
@@ -52,14 +56,14 @@ app.post("/api/notes", function (req, res) {
     const newNote = req.body;
     newNote.id = newId;
     notes.push(newNote);
-    console.log(`Added new note:\n${newNote}`);
+    console.log(`Added new note:\nTitle: ${newNote.title}\nText: ${newNote.text}\nID: ${newNote.id}`);
 
     // Save array of notes to file
     fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), err => {
       if (err) {
         throw err;
       }
-      console.log(`Successfully saved ${notes.length} notes to the database.`);
+      console.log(`Successfully saved ${notes.length} note(s) to the database.`);
 
       // Respond with the newly added note
       res.json(newNote);
